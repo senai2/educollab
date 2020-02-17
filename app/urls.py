35ce67d78a5880435b6c10d831bea84eb6daa54c
jonsnow <abijithanikkuruthi@gmail.com>
2020-02-17 21:27:41 +0100

    login init added

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    # path('login/', views.login),
+    # path('login/', include('django.contrib.auth.urls')),
 ]
