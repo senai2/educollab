ca3508ac9a571867a92ff156788ebf07315879d3
jonsnow <abijithanikkuruthi@gmail.com>
2020-02-22 12:48:41 +0100

    added new tables

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, Image, Field, Topic, Curriculum, Institution
 
 # Register your models here.
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id')
+
+admin.site.register(Institution, InstitutionAdmin)
+
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('name', 'u_id', 'institution', 'designation')
 
 admin.site.register(Member, MemberAdmin)
 
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('title','id')
+
+admin.site.register(Field, FieldAdmin)
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'field', 'id')
+
+admin.site.register(Topic, TopicAdmin)
+
+class CurriculumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'topic','institution', 'id')
+
+admin.site.register(Curriculum, CurriculumAdmin)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.FileField()
+
+    def __str__(self):
+        return self.image.url
+
+class Institution(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
 class Member(models.Model):
     u_id = models.IntegerField() 
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=60)
-    institution = models.CharField(max_length=200)
+    institution = models.ForeignKey(Institution, related_name='member', on_delete=models.CASCADE)
     designation = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
+    # image = models.ForeignKey(Image, related_name='image_path', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['-created_on']
@@ -15,3 +29,23 @@ class Member(models.Model):
     def __str__(self):
         return self.name
 
+class Field(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+    field = models.ForeignKey(Field, related_name='topic', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Curriculum(models.Model):
+    title = models.CharField(max_length=100)
+    topic = models.ForeignKey(Topic, related_name='curriculum', on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, related_name='curriculum', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
