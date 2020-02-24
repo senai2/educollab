aa529e418b8c54d0c4af36c222dc47968ea61c26
jonsnow <abijithanikkuruthi@gmail.com>
2020-02-24 20:37:56 +0100

    media handling added

--- educollab/urls.py
+++ educollab/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .settings import MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
 
 admin.site.site_header = 'EduCollab Administration'
 admin.site.site_title = 'EduCollab Administration'
