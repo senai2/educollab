438d7df0624818dcb83c021b822147b51b59cd15
jonsnow <abijithanikkuruthi@gmail.com>
2020-02-24 20:56:37 +0100

    static hosting

--- educollab/settings.py
+++ educollab/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q!u$khe9soc9!nqoj^g*impznle&isw5(03k5vu%v#9x@=o$=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://abijithanikkuruthi.github.io/educollab/staticfiles/'
+if DEBUG:
+    STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
