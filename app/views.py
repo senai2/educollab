81cdc65540a1ef8fa36eb93c0fff40392c2d70ec
Leonard Herold <git@hdld.me>
2020-03-04 00:37:54 +0100

    Add layout for feeds

--- app/views.py
+++ app/views.py
@@ -2,13 +2,34 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .utils import check_user_id
 from .forms import SignUpForm
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     current_user = request.user
+
+    # todo: bind to some model / facade design pattern
+    # so logs and and other stuff can be combined
+    # also -> showing "bits" of a curriculum is difficult
+    # rather link to that view
+    feeds = { "1":
+            {
+                "title": "Log Entry Title A - Curriculum Updated",
+                "text":  "lorem ispum",
+                "date": datetime.now()
+            },
+        "2":
+            {
+                "title": "Log Entry Title B - Curriculum Created",
+                "text":  "lorem ispum",
+                "date": datetime.now()
+            }
+    }
     if current_user.id:
         check_user_id(current_user)
-        return render(request, 'feeds.html', {})
+        return render(request, 'feeds.html', {
+            "feeds": feeds
+        })
     else:
         return render(request, 'index.html', {})
 
