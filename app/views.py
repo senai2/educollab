d570def2ac5be4b501a307c248daf50ef408b8f2
Leonard Herold <git@hdld.me>
2020-03-05 22:05:27 +0100

    Fix feeds

--- app/views.py
+++ app/views.py
@@ -3,40 +3,27 @@ from django.shortcuts import render, redirect
 from .utils import check_user_id, create_member_obj
 from .forms import SignUpForm
 from datetime import datetime
-from app.models import Field, Subject
+from app.models import Field, Subject, ChangeLog
 from app import curriculum
 
 # Create your views here.
+
+
 def index(request):
     current_user = request.user
-
-    # todo: bind to some model / facade design pattern
-    # so logs and and other stuff can be combined
-    # also -> showing "bits" of a curriculum is difficult
-    # rather link to that view
-    feeds = { "1":
-            {
-                "title": "Log Entry Title A - Curriculum Updated",
-                "text":  "lorem ispum",
-                "date": datetime.now()
-            },
-        "2":
-            {
-                "title": "Log Entry Title B - Curriculum Created",
-                "text":  "lorem ispum",
-                "date": datetime.now()
-            }
-    }
     if current_user.id:
         check_user_id(current_user)
-        return render(request, 'feeds.html', {
-            "feeds": feeds
-        })
+        # TODO: filter only based on subscriptiuos of user
+        changelogs = ChangeLog.objects.all
+        context = {"changelogs": changelogs}
+        return render(request, 'feeds.html', context)
     else:
         return render(request, 'index.html', {})
 
+
 def profile(request):
     return render(request, 'profile.html', {})
+
 
 def signup(request):
     if request.user.is_authenticated:
